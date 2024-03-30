603413d2a6c312a193da4778a48728737f4e806d
hamzaazzam274 <149417796+hamza27azzam@users.noreply.github.com>
2024-03-30 02:19:05 +0200

    main colors
    create a main colors

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,11 @@ nav {
     }
   }
 }
+:root {
+  --Primary_Color: #3b3a5d;
+  --Secondary_Color: #ffbb50;
+  --Content_Color: #8f8f8f;
+  --Link_Color: #b16642;
+  --Border_Color: #b16642;
+}
 </style>
