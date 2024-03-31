d1e709b679c82a9bf62a211b05733308fee720cd
Nada <nadakhaled658@yahoo.com>
2024-03-31 16:53:55 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      about_background:'#F2F2F2',
+      link_color: '#b16642',
+    },
     extend: {},
   },
   plugins: [],
