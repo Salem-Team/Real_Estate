0c976d748b637df3345208e5f1bf02be072ad05b
Nada <nadakhaled658@yahoo.com>
2024-04-02 21:18:35 +0200

    third section has been done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,38 @@
 <template>
-  <section class="banner">
-    <!-- Start Landding Banner -->
-    <div class="parent">
-      <v-container>
-        <v-row style="margin-top: 300px !important">
-          <v-col cols="5">
-            <div class="cont d-flex flex-column text-left mt-24">
-              <h4 class="flex flex-row justify-center gap-1 p-1">
-                <span><img src="@/assets/home_icon.svg" alt="" /></span>Real
-                Estate Agency
-              </h4>
-              <h1 class="title">
-                Find the perfect place to Live with your family
-              </h1>
-              <p class="hint mt-5 mb-5">
-                Distinctively re-engineer revolutionary meta-services and
-                premium architectures. Intrinsically incubate.
-              </p>
-              <v-btn
-                variant="outlined"
-                width="220"
-                height="60px"
-                class="bg-white"
-                style="
-                  font-weight: bold;
-                  text-transform: capitalize;
-                  border: none;
-                  padding: 24px, 30px;
-                "
-                >Explore Property
-                <img src="@/assets/home_arrow.svg" alt="" />
-              </v-btn>
-            </div>
-          </v-col>
-          <v-col cols="6">
-            <div class="parent_img relative">
-              <img
-                class="w-full h-fit absolute top--40 left-56"
-                src="@/assets/Image-6.png"
-                alt=""
-              />
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
+  <!-- Start Landding Banner -->
+  <section class="home overflow-hidden w-full">
+    <div class="landing flex flex-column mt-64 ml-10">
+      <div class="text text-left w-5/12 mt-10">
+        <div id="title" class="flex flex-row gap-1 w-7/12 mb-4 p-1">
+          <span><img src="@/assets/home_icon.svg" alt="" /></span>
+          Real Estate Agency
+        </div>
+        <h1 class="mb-4">Find the perfect place to Live with your family</h1>
+        <p class="mb-4">
+          Distinctively re-engineer revolutionary meta-services and premium
+          architectures. Intrinsically incubate.
+        </p>
+        <button type="submit" class="flex flex-row gap-1 mb-20">
+          Explore Property
+          <span
+            ><img class="relative top-1" src="@/assets/home_arrow.svg" alt=""
+          /></span>
+        </button>
+      </div>
+      <div class="image relative w-6/12 ml-60">
+        <img
+          class="absolute left-96 bottom-0.5 mr-1 max-w-full"
+          src="@/assets/Image-6.png"
+          alt=""
+        />
+      </div>
     </div>
     <!-- End Landding Banner -->
   </section>
 
-  <section id="about" class="w-full flex">
+  <section id="about" class="w-full flex m-10">
     <!--Start About Section -->
-    <div class="about_subtitle bg-about_background m-10 p-16 w-7/12">
+    <div class="about_subtitle bg-about_background p-10 w-7/12">
       <!--Start title -->
       <div class="about_title flex flex-row mb-4">
         <img src="@/assets/subtitle_icon.svg" alt="" />
@@ -79,7 +61,7 @@
       <!--End point part -->
     </div>
     <!--End About Subtitle-->
-    <div class="about_video flex flex-row relative">
+    <div class="about_video flex flex-row relative ml-10">
       <img src="@/assets/about_img.svg" alt="" />
       <a href="http://youtube.com" target="_blank" rel="noopener noreferrer"
         ><img class="absolute inset-1/3" src="@\assets\video_icon.svg" alt=""
@@ -87,10 +69,96 @@
     </div>
     <!--End About Section -->
   </section>
+
+  <section id="why_choose" class="w-full">
+    <div class="properties m-10">
+      <!--Start title -->
+      <div
+        class="properties_title flex flex-row mb-4 items-center justify-center"
+      >
+        <img src="@/assets/subtitle_icon.svg" alt="" />
+        <h3><span>03</span> Why Choose</h3>
+        <img src="@/assets/subtitle_icon.svg" alt="" />
+      </div>
+      <div class="property_title mb-4 flex flex-row justify-center">
+        <h1 class="text-center mb-4">
+          Why Choose Our Properties Of Real Estate Industries
+        </h1>
+      </div>
+      <!--End title -->
+      <div class="properties_cards">
+        <div class="row1 flex flex-row mb-4 justify-around gap-9">
+          <div class="item p-5">
+            <img class="mb-4" src="@/assets/why_choose_Icon.svg" alt="" />
+            <div class="content">
+              <h1 class="mb-4">Budget Friendly</h1>
+              <p>
+                Distinctively re-engineer revolutionary meta-services and
+                premium At vero eos et accusamus et iusto odio dignissimos
+                ducimus qui blanditiis
+              </p>
+            </div>
+          </div>
+          <div class="item_big flex flex-row justify-center p-5">
+            <img src="@/assets/why_choose_icon1.svg" alt="" />
+            <div class="content flex flex-col pt-32">
+              <h1 class="mb-4">Property Insurance</h1>
+              <p>
+                Distinctively re-engineer revolutionary meta-services and
+                premium At vero eos et accusamus et iusto odio dignissimos
+                ducimus qui blanditiis
+                <br />
+                <br />
+                Distinctively re-engineer revolutionary meta-services and
+                premium
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row2 flex flex-row mb-4 jusitfy-around gap-10">
+          <div class="item_bg p-5">
+            <img class="mb-4" src="@/assets/why_choose_icon2.svg" alt="" />
+            <div class="content">
+              <h1 class="mb-4">Trusted By Thousands</h1>
+              <p class="mb-4">
+                Distinctively re-engineer revolutionary meta-services and
+                premium At vero eos et accusamus et iusto odio dignissimos
+                ducimus qui blanditiis
+              </p>
+            </div>
+          </div>
+          <div class="item p-5">
+            <img
+              class="mb-4 mt-10"
+              src="@/assets/why_choose_icon3.svg"
+              alt=""
+            />
+            <div class="content mt-10">
+              <h1 class="mb-4">Prime Location</h1>
+              <p>
+                Distinctively re-engineer revolutionary meta-services and
+                premium At vero eos et accusamus et iusto odio dignissimos
+                ducimus qui blanditiis
+              </p>
+            </div>
+          </div>
+          <div class="item p-5">
+            <img class="mb-4" src="@/assets/why_choose_icon4.svg" alt="" />
+            <div class="content">
+              <h1 class="mb-4">Lowest Commission</h1>
+              <p>
+                Distinctively re-engineer revolutionary meta-services and
+                premium At vero eos et accusamus et iusto odio dignissimos
+                ducimus qui blanditiis
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-// @ is an alias to /src
-
 export default {
   name: "HomeView",
   components: {},
@@ -102,35 +170,51 @@ body {
   margin: 0;
 }
 //Start Landding Banner
-.banner {
-  .parent {
-    background-image: url("../assets/Background-1.jpg");
-    background-size: cover;
-    height: fit-content;
-    width: 100%;
-    overflow: hidden;
-    .cont {
-      h4 {
-        background-color: var(--Background_color);
-        width: fit-content;
-        color: var(--Border_Color);
-        line-height: 23.7px;
-        text-transform: uppercase;
-        letter-spacing: 3px;
-        font-size: 17px;
-        font-weight: lighter;
-        margin-bottom: 30px;
-        font-family: kufam;
-      }
-      h1 {
-        color: var(--Primary_Color);
-        font-size: 55px;
-        line-height: 77px;
-      }
-      p.hint {
-        font-size: 15px;
-        color: var(--Border_Color);
-      }
+.home {
+  background: url(/src/assets/Background-1.jpg) no-repeat center;
+  background-size: cover;
+  height: fit-content;
+  #title {
+    background: linear-gradient(
+      270deg,
+      #f3d1c1 -3.75%,
+      rgba(255, 178, 141, 0) 108.58%
+    );
+    line-height: 23.7px;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-size: 17px;
+    font-family: kufam;
+    color: var(--Border_Color);
+  }
+  .text {
+    h1 {
+      font-family: Kufam;
+      font-size: 55px;
+      font-weight: 700;
+      line-height: 77.02px;
+      text-align: left;
+      color: var(--Primary_Color);
+    }
+    p {
+      font-family: Source Sans 3;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 23.7px;
+      text-align: left;
+      color: var(--Border_Color);
+    }
+    button {
+      padding: 24px 30px;
+      border-radius: 5px;
+      font-family: "Kufam";
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 23.7px;
+      text-align: center;
+      border: none;
+      color: var(--Primary_Color);
+      background-color: white;
     }
   }
 }
@@ -141,6 +225,7 @@ body {
 
 .about_subtitle {
   height: 449px;
+  max-height: fit-content;
   .about_title {
     h3 {
       font-family: Source Sans 3;
@@ -174,13 +259,83 @@ body {
   .points {
     color: var(--Text_Color);
   }
-  .about_video {
-    height: 449px;
-  }
 }
 // End about section
 
+#why_choose {
+  .properties_title {
+    h3 {
+      font-family: Source Sans 3;
+      font-size: 20px;
+      font-weight: 400;
+      line-height: 38.5px;
+      letter-spacing: 0.3em;
+      text-align: left;
+      span {
+        color: var(--Secondary_Color);
+      }
+    }
+  }
+  .property_title {
+    max-height: fit-content;
+    max-width: 100%;
+    h1 {
+      font-family: Kufam;
+      font-size: 45px;
+      font-weight: 700;
+      line-height: 53.32px;
+      text-align: center;
+      width: 650px;
+      height: 105px;
+    }
+  }
+}
+
+.properties_cards {
+  .row1 .item,
+  .row2 .item {
+    background: url(/src/assets/why_choose_background_color.svg) no-repeat right
+      bottom / cover;
+    height: 464px;
+    max-height: fit-content;
+    width: 442px;
+    max-width: fit-content; /* This is to make sure the card doesn't go off screen on smaller screens */
+  }
+  .item_big {
+    background: url(/src/assets/why_choose_background.svg) no-repeat right
+      bottom / cover;
+    width: 921px;
+    height: 464px;
+    max-height: fit-content;
+  }
+  .item_bg {
+    background: url(/src/assets/why_choose_background1.svg) no-repeat right
+      bottom / cover;
+    height: 464px;
+    max-height: fit-content;
+    width: 442px;
+    max-width: fit-content; /* This is to make sure the card doesn't go off screen on smaller screens */
+  }
+  .content {
+    height: 150px;
+    h1 {
+      font-family: Kufam;
+      font-size: 25px;
+      font-weight: 600;
+      line-height: 29.62px;
+      text-align: left;
+      color: var(--Primary_Color);
+    }
+    p {
+      font-family: Source Sans 3;
+      color: var(--Border_Color);
+    }
+  }
+}
+
 @media screen and (max-width: 900px) {
+  .home {
+  }
   #about {
     display: flex;
     flex-direction: column;
