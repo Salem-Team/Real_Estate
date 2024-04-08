09e055dc9108b717b6fe22489fbb3326eda40cf9
Nada <nadakhaled658@yahoo.com>
2024-04-08 10:25:37 +0200

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     colors: {
       about_background: "#F2F2F2",
       link_color: "#b16642",
+      sidebar_border: "#DAC5BB",
+      sidebar_color: "#F5F1EF",
+      primary_color: "#3b3a5d",
+      Secondary_Color: "#ffbb50",
     },
     extend: {},
   },
